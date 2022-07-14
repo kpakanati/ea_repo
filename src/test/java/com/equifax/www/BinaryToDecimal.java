4ac13a73b846462b112c82dc7b41e228aabd50d8
kpakanati <109096669+kpakanati@users.noreply.github.com>
2022-07-14 14:18:48 -0400

    Updated

--- src/test/java/com/equifax/www/BinaryToDecimal.java
+++ src/test/java/com/equifax/www/BinaryToDecimal.java
@@ -5,7 +5,7 @@ public class BinaryToDecimal {
 		int input=11101;
 		double decimal=0;
 		int i=0;
-		while(input>10) {
+		while(input>=10) {
 			int binaryValue=input%10;
 			input=input/10;
 			decimal=decimal+(Math.pow(2,i))*binaryValue;
